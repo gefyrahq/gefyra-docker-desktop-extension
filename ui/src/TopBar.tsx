838da79965f8514329d111163be3ac3b7f13223f
Robert Stein <robert@blueshoe.de>
2022-12-23 17:52:19 +0100

    chore: fix image loading issues
    Signed-off-by: Robert Stein <robert@blueshoe.de>

--- ui/src/TopBar.tsx
+++ ui/src/TopBar.tsx
@@ -1,5 +1,6 @@
+import { ReactComponent as Logo } from "./assets/gefyra_horizontal.svg"
 import { createDockerDesktopClient } from "@docker/extension-api-client";
-import { Button, Grid, Typography, Stepper, Step, StepButton, StepLabel } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 
 export function TopBar() {
     const docker = createDockerDesktopClient();
@@ -11,11 +12,11 @@ export function TopBar() {
         docker.host.openExternal("https://github.com/gefyrahq/gefyra-docker-desktop-extension/issues");
     }
     return (
-        <>
+        <Grid container>
             <Grid item xs={8}>
-                <img src="/assets/gefyra_horizontal.svg" width="200" alt="" />
+                <Logo width="200" height="60"/>
             </Grid>
-            <Grid item xs={4} textAlign="right">
+            <Grid item xs={4} textAlign="right" alignSelf={"flex-end"}>
                 <Button sx={{marginRight: 2}} variant="contained" onClick={githubExtensionLink}>
                     Report an Issue
                 </Button>
@@ -23,11 +24,11 @@ export function TopBar() {
                     Github
                 </Button>
             </Grid>
-            <Grid item xs={12}>
+            <Grid item xs={12} sx={{marginTop: 1}}>
                 <Typography variant="body1" fontWeight={600}>
                     Run local containers in Kubernetes environments.
                 </Typography>
             </Grid>
-        </>
+        </Grid>
     );
 }
