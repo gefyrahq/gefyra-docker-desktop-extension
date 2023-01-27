793393cc62b036d2eba375650f36f2a7f52c3a01
Robert Stein <robert@blueshoe.de>
2023-01-27 15:21:57 +0100

    chore: bump version to 1.0.2 / use in sentry
    Signed-off-by: Robert Stein <robert@blueshoe.de>

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -7,10 +7,10 @@ import { DockerMuiThemeProvider } from '@docker/docker-mui-theme';
 import { App } from './App';
 import { Provider } from 'react-redux';
 import store from './store';
-
+const pjson = require('../package.json');
 Sentry.init({
   dsn: 'https://11cee47c7bdd4a2a91e211b2119cb8fb@sentry.unikube.io/6',
-  release: process.env.REACT_APP_VERSION,
+  release: pjson.version,
   environment: process.env.NODE_ENV,
   beforeSend(event) {
     if (event.user) {
