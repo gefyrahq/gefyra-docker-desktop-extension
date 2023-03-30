37e61eca21af0b7808a3752246b97720d9df82ad
Robert Stein <robert@blueshoe.de>
2023-03-30 14:41:29 +0200

    fix: new typing for datagrid

--- ui/src/Home.tsx
+++ ui/src/Home.tsx
@@ -156,17 +156,19 @@ export function Home() {
             />
           </FormGroup>
         </Grid>
-        <DataGrid
-          rows={containers}
-          columns={columns}
-          pageSize={5}
-          autoHeight={true}
-          checkboxSelection={false}
-          getRowId={getRowId}
-          disableSelectionOnClick={true}
-          onRowClick={openContainer}
-          localeText={{ noRowsLabel: 'No containers found' }}
-        />
+        <div style={{ width: '100%' }}>
+          <DataGrid
+            rows={containers}
+            columns={columns}
+            autoHeight={true}
+            hideFooterPagination={true}
+            checkboxSelection={false}
+            getRowId={getRowId}
+            disableRowSelectionOnClick={true}
+            onRowClick={openContainer}
+            localeText={{ noRowsLabel: 'No containers found' }}
+          />
+        </div>
       </Grid>
 
       <Grid item xs={12} container></Grid>
