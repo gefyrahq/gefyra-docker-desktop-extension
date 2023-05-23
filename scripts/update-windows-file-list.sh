88d69cb1186ce83e08700bd9f73375ea6e62dc62
Robert Stein <robert@blueshoe.de>
2023-05-23 16:02:37 +0200

    chore: add missing dirs

--- scripts/update-windows-file-list.sh
+++ scripts/update-windows-file-list.sh
@@ -2,7 +2,7 @@ wget -qO- https://api.github.com/repos/gefyrahq/gefyra-ext/releases/tags/$1 | gr
 
 
 # Extract file names from zip archive and convert to JSON format
-file_list=$(unzip -l ./gefyra-$1-windows-x86_64.zip | awk 'NR>3 && $1 !~ /-/ && $NF !~ /\/$/ && $NF !~ /^files$/{gsub(/\\/,"\\\\",$NF); gsub(/"/,"\\\"",$NF); gsub(/^dist\//,"/windows/",$NF); print "{\"path\": \"" $NF "\"},"}' | sed '$s/,$//' | tr -d '\n')
+file_list=$(unzip -l ./gefyra-$1-windows-x86_64.zip | awk 'NR>3 && $1 !~ /-/ && $NF !~ /^files$/{gsub(/\\/,"\\\\",$NF); gsub(/"/,"\\\"",$NF); gsub(/^dist\//,"/windows/",$NF); print "{\"path\": \"" $NF "\"},"}' | sed '$s/,$//' | tr -d '\n')
 
 # Update metadata.json with the new file list
 jq --argjson list "[$file_list]" '.host.binaries[0].windows = $list' metadata.json > metadata.tmp && mv metadata.tmp metadata.json
